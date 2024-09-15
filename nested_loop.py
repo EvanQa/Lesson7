51f209ff2278db52eaea8cd58e4b442e7a8b36c2
EvanQa <ivan.khri1488@gmail.com>
2024-09-15 21:30:55 +0300

    loops_else, randoms

---
+++ nested_loop.py
@@ -0,0 +1,25 @@
+#input 5 heights from user
+#the height must be between 0.4 - 2.5
+
+# for _ in range(5):
+#     height: float = float(input("enter height:"))
+#     while not 0.4 <= height <= 2.5:
+#         height: float = float(input("enter height:"))
+#     else:
+#         print(f"{height}, is correct")
+#
+
+# x: int = int(input("enter number"))
+#
+# for _ in range(x):
+#     for i in range(x):
+#         print("*", end = " ")
+#     print()
+
+
+for i in range(1, 4+ 1):
+    for j in range(1, i + 1):
+        print("*", end = " ")
+    print()
+
+
--- ran_dom.py
+++ ran_dom.py
@@ -1,4 +1,41 @@
 import random
+from calendar import MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+from random import choice
 
-for _ in range(10):
-    print(random.randint(a = 10, b = 12), end = " ")
+#
+# for _ in range(10):
+#     print(random.randint(a = 10, b = 12), end = " ")
+#
+# first way
+# num1: int = random.randint(a = 0, b = 100)
+# num2: int = random.randint(a = 0, b = 100)
+# print(f"first  number is {num1}")
+# print(f"second  number is {num2}")
+# sum_num: int = (num1 + num2)
+# answer: int = int(input(f"{num1} + {num2} ="))
+# if answer != sum_num:
+#     print("you're wrong")
+# else:
+#     print("you're correct")
+#
+#
+# #second way with while
+# num1: int = random.randint(a = 0, b = 100)
+# num2: int = random.randint(a = 0, b = 100)
+# print(f"first  number is {num1}")
+# print(f"second  number is {num2}")
+# sum_num: int = (num1 + num2)
+# answer: int = int(input(f"{num1} + {num2} ="))
+# while answer != sum_num:
+#     print("you're wrong")
+#     answer: int = int(input(f"{num1} + {num2} ="))
+# else:
+#     print("you're correct")
+#
+# choice: int = random.randint(1,2)
+# if choice == 1:
+#     print(True)
+# else:
+#     print(False)
+
+print("random day of week: ", random.choice( ["MONDAY","TUESDAY","WEDNESDAY", "THURSDAY","FRIDAY","SATURDAY", "SUNDAY"]))
